89db11c0c1dce4cc688b7b2097af978c27ef31a5
JOSUE_EZEKIEL_DELGADO_VALENZUELA <a.20308051280491@cbtis128.edu.mx>
2023-04-19 15:15:17 +0000

    Version 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -8,35 +9,115 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // Application name
       title: 'Flutter Hello World',
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter GridView'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+        appBar: AppBar(
+          // The title text which will be shown on the action bar
+          title: Text(title),
         ),
-      ),
-    );
+        body: GridView(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              //Numero de columnas:
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 20,
+              mainAxisExtent: 200,
+              childAspectRatio: 1),
+          children: [
+            Container(
+              child: Column(
+                children: [
+                  Image.asset("assets/images/img1.jpg"),
+                  Text("Texto 1")
+                ],
+              ),
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Image.asset("assets/images/img2.jpg"),
+                  Text("Texto 2")
+                ],
+              ),
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Image.asset("assets/images/img3.jpg"),
+                  Text("Texto 3")
+                ],
+              ),
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Image.asset("assets/images/img4.jpg"),
+                  Text("Texto 4")
+                ],
+              ),
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Image.asset("assets/images/img5.jpg"),
+                  Text("Texto 5")
+                ],
+              ),
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Image.asset("assets/images/img6.jpg"),
+                  Text("Texto 6")
+                ],
+              ),
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Image.asset("assets/images/img7.jpg"),
+                  Text("Texto 7")
+                ],
+              ),
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Image.asset("assets/images/img8.jpg"),
+                  Text("Texto 8")
+                ],
+              ),
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Image.asset("assets/images/img9.jpg"),
+                  Text("Texto 9")
+                ],
+              ),
+            ),
+          ],
+          padding: EdgeInsets.all(10),
+          shrinkWrap: true,
+        ));
   }
 }
